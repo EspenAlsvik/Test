43c2d1cab45956dbfce7290fcc3d69bf68e6fe1e
Espen Alsvik <Alsvik.espen@gmail.com>
2024-04-02 23:16:39 +0200

    Initial Commit

---
+++ SliderCount/SliderCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderCountApp.swift
+//  SliderCount
+//
+//  Created by Espen Alsvik on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
